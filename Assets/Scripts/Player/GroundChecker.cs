1efb356875913071dd91936addd6ea87a95eec34
Mực <83195998+Muc2k1@users.noreply.github.com>
2022-05-18 15:58:27 +0700

    + Add lights, Testing character

---
+++ Assets/Scripts/Player/GroundChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    private PlayerController parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponentInParent<PlayerController>();
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Ground"))
+        {
+            parent.isOnGround = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Ground"))
+        {
+            parent.isOnGround = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/WallChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallChecker : MonoBehaviour
+{
+    private PlayerController parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponentInParent<PlayerController>();
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Ground"))
+        {
+            parent.isNextToWall = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Ground"))
+        {
+            parent.isNextToWall = false;
+        }
+    }
+}
