de45ca187cd62d1c738b798049ab3765d134edee
Mực <83195998+Muc2k1@users.noreply.github.com>
2022-05-18 18:35:58 +0700

    + Add pickup - unlock system

---
+++ Assets/Scripts/Objects/Cage/Cage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cage : MonoBehaviour
+{
+    [SerializeField] GameObject soul;
+    public bool canInteract = false;
+    GameObject noti;
+    public int skillIndex;
+    void Start() {
+        noti = GetComponentInChildren<NotiObject>().gameObject;
+    }
+    void Update() {
+        if(Input.GetKey("c"))
+        {
+            Debug.Log("Cage: Pick up");
+            soul.SetActive(false);
+            noti.SetActive(false);
+            CustomEvents.OnPlayerUnlock?.Invoke(skillIndex);
+        }    
+    }
+}
---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public bool[] unlockSkill = new bool[6] {false, false, false, false, false, false};
+    //                                        red    blue   yel    vio    ora    gre
+
+    void Start() {
+        CustomEvents.OnPlayerUnlock += UnlockSkill;
+    }
+    void OnDestroy() {
+        CustomEvents.OnPlayerUnlock -= UnlockSkill;
+    }
+    void UnlockSkill(int index)
+    {
+        unlockSkill[index] = true;
+        Debug.Log("PlayerData: unlocked " + unlockSkill);
+    }
+}
