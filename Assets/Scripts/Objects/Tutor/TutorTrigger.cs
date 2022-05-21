c77104b7983023d593f6a61b70ffca82959b43fa
Mực <83195998+Muc2k1@users.noreply.github.com>
2022-05-22 06:30:48 +0700

    + Add tutor prefab

---
+++ Assets/Scripts/Objects/Tutor/TutorTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorTrigger : MonoBehaviour
+{
+    private GameObject text; 
+    void Start()
+    {
+        text = transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            text.SetActive(true);
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            // text.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
