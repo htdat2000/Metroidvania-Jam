d8ebc1d7987dd9a1dd5095b68811cc7c236604bf
Mực <83195998+Muc2k1@users.noreply.github.com>
2022-05-28 15:57:42 +0700

    + Add animation, sprite sheet

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
     [Header("Const")]
     private const float ANTI_SLIDE_ON_FLOOR = 0.05f;
     private const float MAX_FLOOR_SPEED = 20f;
-    private const int DASH_TIME = 1;
+    private const float DASH_TIME = 0.15f;
 
 
     private bool isFacingRight = true;
@@ -33,6 +33,7 @@ public class PlayerController : MonoBehaviour
     private enum State
     {
         Normal,
+        Sliding,
         Rolling,
         Dashing,
         Attacking
@@ -139,6 +140,11 @@ public class PlayerController : MonoBehaviour
                 currentState = State.Attacking;
                 Invoke("BackToNormal", 0.5f);
                 break;
+                case ColorForm.Blue:
+                anim.Play("BParry");
+                currentState = State.Attacking;
+                Invoke("BackToNormal", 0.5f);
+                break;
             }
         }
     }
@@ -182,16 +188,22 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(dashSpeed, 0);
         else
             rb.velocity = new Vector2(-dashSpeed, 0);
-        //anim.Play("WDash");
+        anim.Play("RDash");
         currentState = State.Dashing;
         Invoke("BackToNormal", DASH_TIME);
     }
 
     void SlideCheck()
     {
-        if(isNextToWall && rb.velocity.y <0)
+        if(isNextToWall && rb.velocity.y <0 && !isOnGround)
         {
+            currentState = State.Sliding;
+            anim.Play("Slide");
             rb.velocity = new Vector2(0, -slideSpeed);
+        }
+        else if(currentState == State.Sliding)
+        {
+            currentState = State.Normal;
         }
     }
 
