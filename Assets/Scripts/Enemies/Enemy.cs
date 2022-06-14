9b0a68c529b56d4c51eb4415a318460758001d8d
htdat2000 <12a6.ht.dat@gmail.com>
2022-06-14 20:46:49 +0700

    DraftCatCode

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -116,4 +116,15 @@ public class Enemy : MonoBehaviour, IDamageable
     {
         isMoveable = true;
     }
+
+    protected virtual void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            IDamageable player = col.gameObject.GetComponent<IDamageable>();
+            player.TakeDmg(dmg, this.gameObject);
+            CustomEvents.OnScreenShakeDanger?.Invoke(GameConst.SHAKE_DANGER_AMOUNT, GameConst.SHAKE_DANGER_TIME);
+            EffectPool.Instance.GetHitEffectInPool(col.gameObject.transform.position);
+        }
+    }
 }
