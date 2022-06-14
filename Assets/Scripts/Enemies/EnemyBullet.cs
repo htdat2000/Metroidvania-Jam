5e5ddbc6ffb36af0277c2b541362e5d016480083
htdat2000 <12a6.ht.dat@gmail.com>
2022-06-14 21:44:28 +0700

    CatShoot

---
+++ Assets/Scripts/Enemies/EnemyBullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : EnemyAttack
+{
+    protected Vector2 dir;
+    protected float speed = 3;
+
+    protected void Start()
+    {
+        SetDir();
+    }
+
+    protected void Update()
+    {
+        Move();
+    }
+
+    protected void Move()
+    {
+        transform.Translate(dir * speed * Time.deltaTime, Space.World);
+    }
+
+    public void SetDir()
+    {
+        dir = (WorldManager.Instance.player.transform.position - this.gameObject.transform.position).normalized;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D col)
+    {
+        base.OnTriggerEnter2D(col);
+        Destroy(this.gameObject);
+    }
+}
