5171e64a8c61552ffcd79aec5c127dd5edc25701
htdat2000 <12a6.ht.dat@gmail.com>
2022-07-30 19:27:47 +0700

    SetupSceneTutorAgain

---
+++ Assets/Scripts/Managers/SceneLoadingManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadingManager : MonoBehaviour
+{
+    protected void Start()
+    {
+        //SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/Objects/Others/LoadingPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingPoint : MonoBehaviour
+{   
+    enum MapName{
+        Null,
+        BossDogArea1,
+        BossDogArea2,
+        BossDogArea3,
+        BossDogArea4,
+        BossDogArea5,
+    }
+    [SerializeField] MapName loadMap;
+    [SerializeField] MapName unloadMap;
+
+    protected void OnTriggerEnter2D(Collider2D col)
+    {
+        if(loadMap.ToString() != "Null")
+        {
+            SceneManager.LoadSceneAsync(loadMap.ToString(), LoadSceneMode.Additive);
+        }
+        else
+        {
+            SceneManager.UnloadSceneAsync(unloadMap.ToString());
+        }
+    }
+}
